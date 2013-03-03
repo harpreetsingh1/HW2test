4e63def7b9aa2d5e66af03ebe65845a0a2706a62
Harpreet Singh <harpreet-singh-1@uiowa.edu>
2013-03-03 15:25:49 -0800

    Update movies_controller.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,5 @@
+
 class MoviesController < ApplicationController
-  helper_method :sort_column, :sort_direction
 
   def show
     id = params[:id] # retrieve movie ID from URI route
@@ -8,21 +8,39 @@ class MoviesController < ApplicationController
   end
 
   def index
-    #@movies = Movie.all
-    @movies = Movie.order(sort_column + " " + sort_direction).filter(filter_selection)
-    @all_ratings = Movie.all_ratings
-    @selected_ratings = (params[:ratings].present? ? params[:ratings] : [])
-    session[:p] = params
+sort = params[:sort] || session[:sort]
+ case sort
+   when 'title'
+      ordering,@title_header = {:order => :title}, 'hilite'
+   when 'release_date'
+     ordering,@date_header = {:order => :release_date}, 'hilite'
+ end
+ @selected_ratings = params[:ratings] || session[:ratings] || {}
+ @all_ratings = Movie.all_ratings
+    if @selected_ratings == {}
+            @selected_ratings = Hash[@all_ratings.map {|rating| [rating, rating]}]
+               end
+     if params[:sort] != session[:sort]
+           session[:sort] = sort
+                 redirect_to :sort => sort, :ratings => @selected_ratings and return
+                     end
+     if params[:ratings] != session[:ratings] and @selected_ratings != {}
+            session[:sort] = sort
+            session[:ratings] = @selected_ratings
+            redirect_to :sort => sort, :ratings => @selected_ratings and return
+    end
+ @movies = Movie.find_all_by_rating(@selected_ratings.keys, ordering)
   end
 
   def new
+    session.clear
     # default: render 'new' template
   end
 
   def create
     @movie = Movie.create!(params[:movie])
     flash[:notice] = "#{@movie.title} was successfully created."
-    redirect_to movies_path (session[:p])
+    redirect_to movies_path
   end
 
   def edit
@@ -40,21 +58,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
-    redirect_to movies_path (session[:p])
-  end
-  
-  private
-  
-  def filter_selection
-    params[:ratings] == nil ? Movie.all_ratings : params[:ratings]
-  end
-  
-  def sort_column
-    Movie.column_names.include?(params[:sort]) ? params[:sort] : "id"
-  end
-  
-  def sort_direction
-    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+    redirect_to movies_path
   end
 
 end
