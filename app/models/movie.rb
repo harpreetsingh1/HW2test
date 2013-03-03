d2389d1d5350e7d412e4d14d9b2da0e2fda14e4d
Harpreet Singh <harpreet-singh-1@uiowa.edu>
2013-03-03 15:27:25 -0800

    Update movie.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,5 @@
 class Movie < ActiveRecord::Base
-  def self.all_ratings
-    self.select(:rating).map(&:rating).uniq
-  end
-  
-  def self.filter(selection)
-    begin
-      self.where(:rating => selection.keys)
-    rescue
-      self.where(:rating => selection)
-    end
-  end
+def self.all_ratings
+    %w(G PG PG-13 NC-17 R)
+end
 end
